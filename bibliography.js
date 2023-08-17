d6f21dbc02d39b6767239b420128b6767b7ab2f6
Hashimoto, Yasuhiro <yasuhirohashimoto@gmail.com>
2023-08-17 14:49:51 +0900

    Update bibliography.js

--- bibliography.js
+++ bibliography.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(e) {
+document.addEventListener('DOMContentLoaded', function (e) {
 
     // Define the style of bibliography.
     // [1] ...
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function(e) {
     d3.select('head')
         .append('style')
         .html('#bibliography ol li {list-style:none}'
-              +'#bibliography ol {counter-reset:list; padding:0; margin:0}'
-              +'#bibliography ol > li {display:table; counter-increment:list; margin-bottom: 0.6em;}'
-              +'#bibliography ol > li:before {content:"[" counter(list,decimal) "] "; display:table-cell; padding-right: .6em}');
+            + '#bibliography ol {counter-reset:list; padding:0; margin:0}'
+            + '#bibliography ol > li {display:table; counter-increment:list; margin-bottom: 0.6em;}'
+            + '#bibliography ol > li:before {content:"[" counter(list,decimal) "] "; display:table-cell; padding-right: .6em}');
 
     // Define the style of anchors.
     // ...^[1]
     const format_anchor = (contents) => {
-        return '<sup>['+contents+']</sup>'
+        return '<sup>[' + contents + ']</sup>'
     };
 
     const placeholder = d3.select('#bibliography');
@@ -22,29 +22,28 @@ document.addEventListener('DOMContentLoaded', function(e) {
     // Find bibliography.
     const bib_items = {};
     d3.selectAll('[id^=bib-]')
-    .each(function() {
-        const tag = this.id.slice(4);
-        const str = this.innerHTML.replace(/<\/?[^>]+(>|$)/g, '')  // HTMLタグを削除
-                                .replace(/\s+/g, ' ')            // 複数の空白を単一の空白に置き換え
-                                .trim();                         // 文字列の先頭と末尾の空白を削除
-        bib_items[tag] = str;
-    }).remove();
+        .each(function () {
+            const tag = this.id.slice(4);
+            const str = this.innerHTML.replace(/<\/?[^>]+(>|$)/g, '')  // HTMLタグを削除
+                .replace(/\s+/g, ' ')            // 複数の空白を単一の空白に置き換え
+                .trim();                         // 文字列の先頭と末尾の空白を削除
+            bib_items[tag] = str;
+        }).remove();
 
     // Find citing positions and insert successive numbers there.
     const index = {};
     let count = 0;
     d3.selectAll('[class^=cite-]')
-        .each(function() {
-            const tag = this.classList[0].substr(5);
-            let j = index[tag];
-            if (j===undefined) {
-                j = index[tag] = ++count;
-                // Append the cited item to the bibliography placeholder.
+        .each(function () {
+            const tag = this.classList[0].slice(5);
+
+            if (!index[tag]) {
+                index[tag] = ++count;
                 list.append('li')
-                    .attr('id', 'bib-'+tag)
+                    .attr('id', 'bib-' + tag)
                     .html(bib_items[tag]);
-                
             }
-            this.outerHTML = format_anchor('<a href="#bib-'+tag+'" title="'+bib_items[tag]+'">'+j+'</a>');
+
+            this.outerHTML = format_anchor(`<a href="#bib-${tag}" title="${bib_items[tag]}">${index[tag]}</a>`);
         });
 });
